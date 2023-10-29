a1c67f039497e3efce61fdf9e0744c6af9ce9130
Guo <17735033716@163.com>
2023-10-29 20:10:36 +0800

    Initial commit

---
+++ src/test/java/com/xmut/controller/Dg_certificateControllerTest.java
@@ -0,0 +1,10 @@
+package com.xmut.controller;
+
+import junit.framework.TestCase;
+
+public class Dg_certificateControllerTest extends TestCase {
+
+    public void testSelectDruggistByUser() {
+
+    }
+}
---
+++ src/test/java/com/xmut/controller/DrugsControllerTest.java
@@ -0,0 +1,7 @@
+package com.xmut.controller;
+
+import junit.framework.TestCase;
+
+public class DrugsControllerTest extends TestCase {
+
+}
